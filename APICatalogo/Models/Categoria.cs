0e5f3377c9d27f5d1e0d07b9ca45ab34161d4662
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-12-14 14:47:22 -0300

    Adding files to github

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,7 @@
+﻿namespace APICatalogo.Models;
+    public class Categoria
+{
+    public  int CategoriaID { get; set; }
+    public string? NomeCategoria { get; set; }
+    public string? ImagemUrl { get; set; }
+}
