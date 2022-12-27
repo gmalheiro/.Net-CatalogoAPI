fe83873964e7c186617e4ea635996d6f2f299112
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-12-27 18:42:46 -0300

    Api Completed

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,105 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriaProduto()
+        {
+            //return _context.Categorias.Include(p => p.Produtos).ToList();
+            return _context.Categorias.Include( p => p.Produtos).Where(c => c.CategoriaID <=5).ToList(); 
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            try
+            {
+                throw new DataMisalignedException();
+                return _context.Categorias.AsNoTracking().ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um problema ao tratar a sua solicitação");
+            }
+        }
+
+        [HttpGet("{id:int}",Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get (int id)
+        {
+            try
+            {
+                var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaID == id);
+
+                if (categoria == null)
+                {
+                    return NotFound($"Categoria {id} não encontrada...");
+                }
+                return Ok(categoria);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um problema ao tratar a sua solicitação");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null)
+                return BadRequest();
+
+            _context.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria",
+            new { id = categoria.CategoriaID });
+        }
+
+        [HttpPut]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Categoria>Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaID == id);
+
+            if (categoria is null)
+            {
+                return NotFound();
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+
+    }
+
+}
+
